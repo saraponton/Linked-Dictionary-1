c7b8c5103365032997e4631d9d8bb881fc76251c
Matheus <mferreira.ieu2017@student.ie.edu>
2019-10-21 12:26:39 +0200

    Basic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    static String path = "/Users/Matheus/Linked/src/com/company/unsorteddict.txt";
+
+
     public static void main(String[] args) {
-	// write your code here
+        makeLinkedList();
+
+    }
+
+
+    public static void makeLinkedList(){
+        BufferedReader reader;
+
+        try {
+
+            reader = new BufferedReader(new FileReader(path));
+            String line = reader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
