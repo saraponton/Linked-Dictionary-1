e61d2cd9f7457527238c82a4e68c9dd3740380aa
Matheus <mferreira.ieu2017@student.ie.edu>
2019-10-26 19:09:41 +0200

    Best version so far

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,82 @@
 package com.company;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-
+    static String alphabet="abcdefghijklmnopqrstuvwxyzé";
     static String path = "/Users/Matheus/Linked/src/com/company/unsorteddict.txt";
+    static LinkedList<LinkedList> dict = new LinkedList<>();
 
 
-    public static void main(String[] args) {
-        makeLinkedList();
-
-    }
 
 
-    public static void makeLinkedList(){
-        BufferedReader reader;
+    public static void main(String args[])
+    {
+        for ( char letter : alphabet.toCharArray()){
+            dict.add(new LinkedList<String>());
+        }
+        File file = new File(path);
 
         try {
 
-            reader = new BufferedReader(new FileReader(path));
-            String line = reader.readLine();
+            Scanner sc = new Scanner(file);
+            int index = 0;
+            long startingTime = System.currentTimeMillis();
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine().toLowerCase();
 
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
+                int indicator = alphabet.indexOf(i.charAt(0));
+
+                int position = getPosition(i, indicator);
+                if(position != -1) {
+                    dict.get(indicator).add(position, i);
+                }
+                else {
+                    dict.get(indicator).add(i);
+                }
+
+
+                index++;
+                System.out.println(index);
 
             }
-            reader.close();
-        } catch (IOException e) {
+            long elapsedTime = (System.currentTimeMillis() - startingTime);
+
+            sc.close();
+            for(LinkedList<String> element : dict){
+                for(String word: element){
+                    System.out.println(word);
+                }
+            }
+            System.out.println(elapsedTime);
+
+
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+
+
     }
+
+    public static int getPosition(String word, int indicator) {
+        String temp = Character.toString(word.charAt(0));
+        int index = dict.get(indicator).indexOf(temp);
+        if (index<0){
+            index = 0;}
+        for(int j=index;j< dict.get(indicator).size();j++) {
+            String word_2 = (String) dict.get(indicator).get(j);
+            if (word.compareTo(word_2) < 0){
+                return j;
+            }
+
+        }
+        return -1;
+    }
+
+
 }
--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class Node {
+    String value;
+    Node next;
+
+    public Node(String value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
 }
