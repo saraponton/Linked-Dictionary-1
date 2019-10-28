e3b019e72082b10895821969c6ddc89f4cc6522b
Matheus <mferreira.ieu2017@student.ie.edu>
2019-10-28 11:40:19 +0100

    Final version b4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -60,14 +60,12 @@ public class Main {
 
             Scanner scan = new Scanner(System.in);
             for (int j = 0;j<10;j++) {
-                x = scan.nextLine();
-                if (toPrint[x] == null && toPrint.indexOf(x)){
-                    System.out.println("-1");
-                }else if (x == (int)x){
+                x = scan.nextInt();
+                if (toPrint[x]== null){
+                }else {
                     System.out.println(toPrint[x]);
-                } else {
-                    toPrint.indexOf(x)
 
+                }
                 }
                 scan.close();
 
